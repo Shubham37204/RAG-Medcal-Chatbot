a32f2cac0a500537bad8cd4b34641f8ac9791abb
Shubham Bhardwaj <shubhambhardwaj9575@gmail.com>
2026-03-22 14:54:12 +0530

    initial clean commit

---
+++ app/config/config.py
@@ -0,0 +1,11 @@
+import os
+HF_TOKEN = os.environ.get("HF_TOKEN")
+if not HF_TOKEN:
+    raise ValueError("HF_TOKEN is not set in environment variables")
+
+
+HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
+DB_FAISS_PATH="vectorstore/db_faiss"
+DATA_PATH="data/"
+CHUNK_SIZE=500
+CHUNK_OVERLAP=50
