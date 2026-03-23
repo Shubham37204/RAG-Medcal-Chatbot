36d64141aeb485b776cd30630cadb84ad3274d91
Shubham Bhardwaj <shubhambhardwaj9575@gmail.com>
2026-03-23 13:58:34 +0530

    embedding code

---
+++ app/components/embedding.py
@@ -0,0 +1,16 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+logger = get_logger(__name__)
+
+def get_embedding_model():
+    try:
+        logger.info("initiazing our hugging face")
+        model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+        logger.info("Hugging embedding model loaded successfully")
+        return model
+    except Exception as e:
+        error_msg=CustomException("error in loading emvedding model",e)
+        logger.error(str(error_msg))
+        raise error_msg
